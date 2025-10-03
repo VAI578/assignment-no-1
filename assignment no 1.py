4864724bb6d42ddcd690ca263e3397cac7cb0196
VAISHNAVI SUNIL PURIGOSAVI <gosavivaishnavi9370@gmail.com>
2025-10-03 23:26:41 +0530

    Add files via upload

---
+++ assignment no 1.py
@@ -0,0 +1,13 @@
+# TASK 1
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2 )
+
+#TASK : 2
+first_name =input("enter your first name")
+last_name =input("enter your last name")
+res = first_name + last_name
+print(res,("welcome to python program"))
